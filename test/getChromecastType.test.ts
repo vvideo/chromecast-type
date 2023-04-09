9bf49e26d03d68864dae7fc418aa6cc56bc70284
Denis Seleznev <hcodes@users.noreply.github.com>
2023-04-09 20:42:38 +0300

    Fix tests

--- test/getChromecastType.test.ts
+++ test/getChromecastType.test.ts
@@ -1,4 +1,4 @@
-import { getChromecastType, getChromecastTypeByUserAgent } from '../src';
+import { getChromecastTypeByUserAgent } from '../src';
 import {
     chromecastAndroidTvUserAgent,
     chromecastUserAgent,
@@ -7,17 +7,25 @@ import {
     firefoxUserAgent,
     safariUserAgent,
     fuchsiaUserAgent,
+    googleNestHub,
+    googleNestHubMax,
 } from '../test/userAgent';
 
 describe('Chromecast', () => {
     it('#getChromecastTypeByUserAgent', () => {
         const variants = [
             [chromecastUserAgent, 'chromecast'],
-            [firefoxUserAgent, ''],
-            [fuchsiaUserAgent, 'google-nest-hub'],
+
             [chromecastUserAgentDeviceType, 'chromecast'],
-            [chromecastWithGoogleTvUserAgent, 'chromecast-with-google-tv'],            [chromecastAndroidTvUserAgent, 'android-tv-with-chromecast'],
+            [chromecastWithGoogleTvUserAgent, 'chromecast-with-google-tv'],
+            [chromecastAndroidTvUserAgent, 'android-tv-with-chromecast'],
+
+            [fuchsiaUserAgent, 'google-nest-hub-2'],
+            [googleNestHub, 'google-nest-hub-1'],
+            [googleNestHubMax, 'chromecast'],
+
             [safariUserAgent, ''],
+            [firefoxUserAgent, ''],
         ];
 
         variants.forEach(([userAgent, expected]) => {
--- test/userAgent.ts
+++ test/userAgent.ts
@@ -2,9 +2,6 @@ export const chromecastUserAgent = 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/
 export const chromecastUserAgentDeviceType = 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.225 Safari/537.36 CrKey/1.56.500000 DeviceType/Chromecast';
 export const chromecastWithGoogleTvUserAgent = 'Mozilla/5.0 (Linux; Android 12.0; Build/STTE.220920.016.H1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 CrKey/1.56.500000 DeviceType/AndroidTV';
 export const chromecastAndroidTvUserAgent = 'Mozilla/5.0 (Linux; Android 9.0; Build/PTM3.190725.169) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.54 Safari/537.36 CrKey/1.47.216070';
-export const chromecastAndroidTv2UserAgent = 'Mozilla/5.0 (Linux; Android) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.109 Safari/537.36 CrKey/1.54.248666';
-export const chromecastAndroidTv3UserAgent = 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320';
-
 export const chromecast1UserAgent = 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.47 Safari/537.36 CrKey/1.36.159268';
 export const chromecast2UserAgent = 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.225 Safari/537.36 CrKey/1.56.500000';
 export const chromecastUltraUserAgent = 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.225 Safari/537.36 CrKey/1.56.500000 DeviceType/Chromecast';
